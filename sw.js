ef0e49bf6723192715a066e89d5412bd3015e9c5
egozhuk <egorvzhukov@gmail.com>
2023-08-07 19:12:27 +0700

    analytics

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-        ]);
-      })
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/account.html',
+                '/icon.png',
+                '/manifest.json',
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
+});
+
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
-  });
+});
